fc0264e9a663652dfc0d81b8a390377e3087fb86
Gekkejager <mickvanhetnederend@gmail.com>
2017-02-26 14:21:28 +0100

    Init

---
+++ src/Assignment1.java
@@ -0,0 +1,11 @@
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		Evaluators fitnessFunctions = new Evaluators();
+		
+		int testArr[] = {1,1,1,1,0,0,0,0};
+		fitnessFunctions.CompareEvaluators(testArr);
+	}
+
+}
---
+++ src/Evaluators.java
@@ -0,0 +1,74 @@
+
+public class Evaluators {
+	
+	public void CompareEvaluators(int[] input){
+		System.out.printf("Uniformly Scaled Counting Ones Function: %.2f\n", UniformlyScaledCountingOnesFunction(input));
+		System.out.printf("Linearly Scaled Counting Ones Function: %.2f\n", LinearlyScaledCountingOnesFunction(input));
+		System.out.printf("Deceptive Trap Function: %.2f\n", DeceptiveTrapFunction(input));
+		System.out.printf("Non-deceptive Trap Function: %.2f\n", NonDeceptiveTrapFunction(input));
+	}
+	
+	// Counts the number of ones
+	public double UniformlyScaledCountingOnesFunction(int[] input){
+		double counter = 0;
+		for(int i = 0; i < input.length; i++){
+			if(input[i] == 1)
+				counter++;
+		}
+		return counter;
+	}
+	
+	// Sums the indices of the ones
+	public double LinearlyScaledCountingOnesFunction(int[] input){
+		double sum = 0;
+		for(int i = 0; i < input.length; i++){
+			if(input[i] == 1)
+				sum += i + 1;
+		}
+		return sum;
+	}
+	
+	// Very difficult function to optimize from
+	public double DeceptiveTrapFunction(int[] input){
+		return TrapFunction(input, 1);
+	}
+	
+	// Somewhat difficult function to optimize from
+	public double NonDeceptiveTrapFunction(int[] input){
+		return TrapFunction(input, 2.5);
+	}
+	
+	
+	// Overload to set k=4
+	private double TrapFunction(int[] input, double deceptionParam){
+		int k = 4;
+		return TrapFunction(input, k, deceptionParam);
+	}
+	
+	// Chunks array into pieces of size K and evaluates them
+	private double TrapFunction(int[] input, int k, double deceptionParam){
+		double sum = 0;
+		
+		for(int i=0; i < Math.floor(input.length/(double) k); i++){
+			int subArray[] = new int[k];
+			for(int j=0; j < k; j++){
+				subArray[j] = input[i*k + j];
+			}
+			sum += TrapSubArray(subArray, k, deceptionParam);
+		}
+				
+		return sum;
+		
+	
+	}
+	
+	// Evaluate sub array according to the trap function
+	private double TrapSubArray(int[] subArray, int k, double deceptionParam){
+		double COScore = UniformlyScaledCountingOnesFunction(subArray);
+		if(COScore == k)
+			return k;
+		
+		return k - deceptionParam - ((k - deceptionParam)/(k - 1)) * COScore;
+	}
+
+}
